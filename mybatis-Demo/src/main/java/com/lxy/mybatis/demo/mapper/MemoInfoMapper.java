58c26420a8da5f7ca08c500a4f98235aa413dc72
403824215@qq.com <403824215@qq.com>
2018-07-09 16:29:12 +0800

    初建Mybatis

--- mybatis-Demo/src/main/java/com/lxy/mybatis/demo/mapper/MemoInfoMapper.java
+++ mybatis-Demo/src/main/java/com/lxy/mybatis/demo/mapper/MemoInfoMapper.java
@@ -1,20 +1,26 @@
 package com.lxy.mybatis.demo.mapper;
 
+import com.lxy.mybatis.demo.common.Page;
 import com.lxy.mybatis.demo.entity.MemoInfo;
 import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 public interface MemoInfoMapper {
+    //插入
     int insertMemoInfo(MemoInfo memoInfo);
 
+    //更新
     int updataMemoInfo(@Param("id") int id,@Param("content") String content);
 
     int updataMemoInfoObject(MemoInfo memoInfo);
 
+    //删除
     int deleteMemoInfo(int id);
 
+    //查询
     MemoInfo queryMemoInfoById(int id);
 
     Map queryMemoInfoByIdReturnHashMap(int id);
@@ -22,6 +28,33 @@ public interface MemoInfoMapper {
     List<MemoInfo> queryMemoInfoAll();
 
     List<Map> queryMemoInfoAllReturnHashMap();
+
+    //分页查询
+    List<MemoInfo> queryMemoInfoByPage(@Param("pageSize") Integer pageSize, @Param("pageOffset") Integer pageOffset, @Param("orderColumnName") String orderColumnName);
+
+    List<MemoInfo> queryMemoInfoByPageObject(Page page);
+
+
+    //练习
+    //根据title和privacy查询便签信息
+    List<MemoInfo> queryMemoInfoAndLikeTitleAndLikePrivacy(@Param("title") String title,@Param("isprotected") Character isprotected);
+
+    //查询创建时间在startTime,endTime之间的便签信息
+    List<MemoInfo> queryMemoInfoBetweenStartAndEnd(@Param("startTime")LocalDateTime startTime, @Param("endTime")LocalDateTime endTime);
+
+    //查询符合title或者content的其中一个条件的便签
+    List<MemoInfo> queryMemoInfoAndLikeTitleOrLikeContent(@Param("title")String title,@Param("content")String content);
+
+    //查询Id在一个集合的便签信息
+    List<MemoInfo> queryMemoInfoInSame(List ids);
+
+    //分页查询便签信息
+    List<MemoInfo> queryMemoInfoByPage1(@Param("pageSize") Integer pageSize, @Param("pageOffset") Integer pageOffset, @Param("orderColumnName") String orderColumnName);
+
+    //根据MemoInfo更新便签信息，更新内容有：group_id , title, content, is_protected, is_remind, remindTime
+   int updataMemoInfoByGroupIdAndTitleAndContentAndIsprotectAndIsremindAndRemindTime
+        (@Param("id") Integer id,@Param("groupid") Integer groupid,@Param("title")String title,@Param("content")String content,@Param("isprotected")Character isprotected,@Param("remind")Character remind,@Param("reminTime") LocalDateTime reminTime);
+
 
 
 }
--- mybatis-Demo/src/test/java/com/lxy/mybatis/mapper/MemoInfoMapperTest.java
+++ mybatis-Demo/src/test/java/com/lxy/mybatis/mapper/MemoInfoMapperTest.java
@@ -1,10 +1,12 @@
 package com.lxy.mybatis.mapper;
 
-
+import com.github.pagehelper.PageHelper;
+import com.lxy.mybatis.demo.common.Page;
 import com.lxy.mybatis.demo.common.backGround;
 import com.lxy.mybatis.demo.entity.MemoInfo;
 import com.lxy.mybatis.demo.mapper.MemoInfoMapper;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -13,23 +15,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-
 public class MemoInfoMapperTest {
     private static SqlSessionFactory sqlSessionFactory;
+
+    //日志
     private final Logger logger = LoggerFactory.getLogger(MemoInfoMapperTest.class);
 
     static{
         try {
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("practise_map.xml"));
+            //sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("practise_map.xml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     //插入测试
     @Test
     public void test_insertMemoInfo()
@@ -37,10 +43,10 @@ public class MemoInfoMapperTest {
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
         MemoInfo memoInfo = new MemoInfo();
-        memoInfo.setGroup_id(7);
+        memoInfo.setGroup_id(3);
         memoInfo.setBackground(backGround.WHITE);
-        memoInfo.setContent("HXF");
-        memoInfo.setTitle("Java");
+        memoInfo.setContent("LLH");
+        memoInfo.setTitle("C++");
         memoInfo.setIsprotected('0');
         memoInfo.setRemind('1');
         memoInfo.setReminTime(new Date());
@@ -101,11 +107,12 @@ public class MemoInfoMapperTest {
         MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
         //MemoInfo memoInfo = new MemoInfo();
 
-        int effect = memoInfoMapper.deleteMemoInfo(5);
-        logger.info("Delete id = 5,Result = {}",effect);
+        int effect = memoInfoMapper.deleteMemoInfo(8);
+        logger.info("Delete id = 8,Result = {}",effect);
 
         sqlSession.close();
     }
+
 
     //查询测试
     @Test
@@ -114,8 +121,8 @@ public class MemoInfoMapperTest {
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
 
-        MemoInfo memoInfo = memoInfoMapper.queryMemoInfoById(4);
-        logger.info("Select id = 4,Result = {}",memoInfo);
+        MemoInfo memoInfo = memoInfoMapper.queryMemoInfoById(3);
+        logger.info("Select id = 3,Result = {}",memoInfo);
 
         sqlSession.close();
     }
@@ -140,10 +147,18 @@ public class MemoInfoMapperTest {
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
 
-        List<MemoInfo> memoInfo = memoInfoMapper.queryMemoInfoAll();
-        logger.info("Select id = 4,Result = {}",memoInfo);
+        List<MemoInfo> memoInfo1 = memoInfoMapper.queryMemoInfoAll();
+        logger.info("first:Select id = 9,Result = {}",memoInfo1);
+
+
+        List<MemoInfo> memoInfo2 = memoInfoMapper.queryMemoInfoAll();
+        logger.info("second:Select id = 9,Result = {}",memoInfo2);
+
+        List<MemoInfo> memoInfo3 = memoInfoMapper.queryMemoInfoAll();
+        logger.info("third:Select id = 9,Result = {}",memoInfo3);
 
         sqlSession.close();
+
     }
 
     //查询测试
@@ -158,6 +173,129 @@ public class MemoInfoMapperTest {
 
         sqlSession.close();
     }
+
+    //分页查询测试
+    @Test
+    public void test_queryMemoInfoByPage()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        int pageSize = 2;
+        String orderColumnName = "id";
+
+        for (int i = 1; i < 3; i++) {
+            int pageNumber = i;
+            int pageOffset = (pageNumber - 1) * pageSize;
+            List<MemoInfo> memoInfo = memoInfoMapper.queryMemoInfoByPage(pageSize, pageOffset, orderColumnName);
+            logger.info("CurrentPage = {} Result = {}", pageNumber, memoInfo);
+        }
+
+        sqlSession.close();
+    }
+
+    //分页查询测试
+    @Test
+    public void test_queryMemoInfoByPageObject()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        Page page = new Page();
+        page.setOrderColumnName("id");
+
+        for (int i = 1; i < 3; i++) {
+            page.setPageNumber(i);
+            List<MemoInfo> memoInfo = memoInfoMapper.queryMemoInfoByPageObject(page);
+            logger.info("CurrentPage = {} Result = {}", page, memoInfo);
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test_queryMemoInfoAndLikeTitleAndLikePrivacy()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        List<MemoInfo> memoInfos = memoInfoMapper.queryMemoInfoAndLikeTitleAndLikePrivacy("Java",'0');
+        logger.info("Result is : {}",memoInfos);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test_queryMemoInfoBetweenStartAndEnd()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+
+        LocalDateTime startTime = LocalDateTime.of(2018,7,1,0,0,0);
+        LocalDateTime endTime = LocalDateTime.now();
+
+        List<MemoInfo> memoInfos = memoInfoMapper.queryMemoInfoBetweenStartAndEnd(startTime,null);
+        logger.info("Result is : {}",memoInfos);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test_queryMemoInfoAndLikeTitleOrLikeContent()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        List<MemoInfo> memoInfos = memoInfoMapper.queryMemoInfoAndLikeTitleOrLikeContent("java",null);
+        logger.info("Result is : {}",memoInfos);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test_queryMemoInfoInSame()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        List<Integer> ids = new ArrayList<Integer>();
+        ids.add(9);
+        ids.add(10);
+        ids.add(11);
+        ids.add(12);
+        logger.info("Result is : {}",ids);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test_queryMemoInfoByPage1()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        PageHelper.startPage(1, 1);
+        List<MemoInfo> list = memoInfoMapper.queryMemoInfoAll();
+
+        logger.info("Result is : {}",list);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test_updataMemoInfoByGroupIdAndTitleAndContentAndIsprotectAndIsremindAndRemindTime()
+    {
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        MemoInfoMapper memoInfoMapper = sqlSession.getMapper(MemoInfoMapper.class);
+
+        int remindTime = memoInfoMapper.updataMemoInfoByGroupIdAndTitleAndContentAndIsprotectAndIsremindAndRemindTime(10,3,"JS","CSS",'0','1',LocalDateTime.now());
+        logger.info("Result is : {}",remindTime);
+
+        sqlSession.close();
+    }
+
+
 
 
 }
