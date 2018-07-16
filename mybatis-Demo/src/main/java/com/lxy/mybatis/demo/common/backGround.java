f44835d7263b430ee11c23412edae1e66930201a
403824215@qq.com <403824215@qq.com>
2018-07-16 11:07:13 +0800

    删除

--- mybatis-Demo/src/main/java/com/lxy/mybatis/demo/common/backGround.java
+++
@@ -1,5 +0,0 @@
-package com.lxy.mybatis.demo.common;
-
-public enum backGround {
-    WHITE,BLACK,RED,BLUE
-}
--- mybatis-Demo/src/main/java/com/lxy/mybatis/demo/mapper/MemoGroupMapper.java
+++
@@ -1,7 +0,0 @@
-package com.lxy.mybatis.demo.mapper;
-
-import com.lxy.mybatis.demo.entity.MemoGroup;
-
-public interface MemoGroupMapper {
-    int insertMemoGroup(MemoGroup memoGroup);
-}
